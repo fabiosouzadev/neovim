adf6afb449bc6206120c331497cf7009e9e5a862
Fabio Souza <fabiovanderlei.developer@gmail.com>
2026-03-26 11:41:02 -0300

    feat(blink.cmp, configs): add initial configs and blink.cmp

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,60 @@
+return {
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    opts = { style = "moon" },
+  },
+    -- catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      flavour = "auto", -- latte, frappe, macchiato, mocha
+      lsp_styles = {
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        fzf = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        snacks = true,
+        telescope = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
+
+    -- rose-pine
+  { 
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "auto", -- auto, main, moon, or dawn
+      dark_variant = "main", -- main, moon, or dawn
+    }
+  }
+}
