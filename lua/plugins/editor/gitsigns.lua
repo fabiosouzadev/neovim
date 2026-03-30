aba9b884611b8b02cb0f21e0019710c76e28913c
Fabio Souza <fabiovanderlei.developer@gmail.com>
2026-03-30 15:53:58 -0300

    fix(gitsigns): Fix gitsigns

--- lua/plugins/editor/gitsigns.lua
+++ lua/plugins/editor/gitsigns.lua
@@ -74,17 +74,17 @@ return {
     end,
   },
 }
-return {
-  "gitsigns.nvim",
-  opts = function()
-    Snacks.toggle({
-      name = "Git Signs",
-      get = function()
-        return require("gitsigns.config").config.signcolumn
-      end,
-      set = function(state)
-        require("gitsigns").toggle_signs(state)
-      end,
-    }):map("<leader>uG")
-  end,
-}
+-- return {
+--   "gitsigns.nvim",
+--   opts = function()
+--     Snacks.toggle({
+--       name = "Git Signs",
+--       get = function()
+--         return require("gitsigns.config").config.signcolumn
+--       end,
+--       set = function(state)
+--         require("gitsigns").toggle_signs(state)
+--       end,
+--     }):map("<leader>uG")
+--   end,
+-- }
