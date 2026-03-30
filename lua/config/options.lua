1a86115d7815ba101594f4d7bdec24844e5fb37f
Fabio Souza <fabiovanderlei.developer@gmail.com>
2026-03-30 11:38:22 -0300

    feat(gitsigns): Add more configs to gitsigns

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,8 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 vim.g.autoformat = true
 
