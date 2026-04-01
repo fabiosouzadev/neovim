890d87ac7049004b5c6f3bd23e29df5ec67fad1c
Fabio Souza <fabiovanderlei.developer@gmail.com>
2026-04-01 16:44:40 -0300

    chore(gitsigns): change gitsigns configs

--- lua/plugins/editor/gitsigns.lua
+++ lua/plugins/editor/gitsigns.lua
@@ -5,90 +5,94 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  "lewis6991/gitsigns.nvim",
-  ---@module 'gitsigns'
-  ---@type Gitsigns.Config
-  ---@diagnostic disable-next-line: missing-fields
-  opts = {
-    signs = {
-      add = { text = "+" }, ---@diagnostic disable-line: missing-fields
-      change = { text = "~" }, ---@diagnostic disable-line: missing-fields
-      delete = { text = "_" }, ---@diagnostic disable-line: missing-fields
-      topdelete = { text = "‾" }, ---@diagnostic disable-line: missing-fields
-      changedelete = { text = "~" }, ---@diagnostic disable-line: missing-fields
-      untracked = { text = "~" }, ---@diagnostic disable-line: missing-fields
-    },
-    signs_staged = {
-      add = { text = "+" }, ---@diagnostic disable-line: missing-fields
-      change = { text = "~" }, ---@diagnostic disable-line: missing-fields
-      delete = { text = "_" }, ---@diagnostic disable-line: missing-fields
-      topdelete = { text = "‾" }, ---@diagnostic disable-line: missing-fields
-      changedelete = { text = "~" }, ---@diagnostic disable-line: missing-fields
-    },
-    on_attach = function(bufnr)
-      local gitsigns = require("gitsigns")
+  {
+    "lewis6991/gitsigns.nvim",
+    ---@module 'gitsigns'
+    ---@type Gitsigns.Config
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      signs = {
+        add = { text = "+" }, ---@diagnostic disable-line: missing-fields
+        change = { text = "~" }, ---@diagnostic disable-line: missing-fields
+        delete = { text = "_" }, ---@diagnostic disable-line: missing-fields
+        topdelete = { text = "‾" }, ---@diagnostic disable-line: missing-fields
+        changedelete = { text = "~" }, ---@diagnostic disable-line: missing-fields
+        untracked = { text = "~" }, ---@diagnostic disable-line: missing-fields
+      },
+      signs_staged = {
+        add = { text = "+" }, ---@diagnostic disable-line: missing-fields
+        change = { text = "~" }, ---@diagnostic disable-line: missing-fields
+        delete = { text = "_" }, ---@diagnostic disable-line: missing-fields
+        topdelete = { text = "‾" }, ---@diagnostic disable-line: missing-fields
+        changedelete = { text = "~" }, ---@diagnostic disable-line: missing-fields
+      },
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
 
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
-      end
-
-      -- Navigation
-      map("n", "]h", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "]c", bang = true })
-        else
-          gitsigns.nav_hunk("next")
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
         end
-      end, { desc = "Jump to next git [c]hange" })
 
-      map("n", "[h", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "[c", bang = true })
-        else
-          gitsigns.nav_hunk("prev")
-        end
-      end, { desc = "Jump to previous git [c]hange" })
+        -- Navigation
+        map("n", "]h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gitsigns.nav_hunk("next")
+          end
+        end, { desc = "Jump to next git [c]hange" })
 
-      -- Actions
-      -- visual mode
-      map("v", "<leader>ghs", function()
-        gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end, { desc = "git [s]tage hunk" })
-      map("v", "<leader>ghr", function()
-        gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end, { desc = "git [r]eset hunk" })
+        map("n", "[h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gitsigns.nav_hunk("prev")
+          end
+        end, { desc = "Jump to previous git [c]hange" })
 
-      -- normal mode
-      map("n", "]H", function()
-        gitsigns.nav_hunk("last")
-      end, { desc = "Last Hunk" })
-      map("n", "[H", function()
-        gitsigns.nav_hunk("first")
-      end, { desc = "First Hunk" })
-      map({ "n", "x" }, "<leader>ghs", gitsigns.stage_hunk, { desc = "git hunk [s]tage" })
-      map({ "n", "x" }, "<leader>ghr", gitsigns.reset_hunk, { desc = "git hunk [r]eset" })
-      map("n", "<leader>ghS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
-      map("n", "<leader>ghu", gitsigns.undo_stage_hunk, { desc = "git [u]ndo stage hunk" })
-      map("n", "<leader>ghR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" })
-      map("n", "<leader>ghp", gitsigns.preview_hunk, { desc = "git [p]review hunk" })
-      map("n", "<leader>ghi", gitsigns.preview_hunk_inline, { desc = "[T]oggle git show [D]eleted" })
+        -- Actions
+        -- visual mode
+        map("v", "<leader>ghs", function()
+          gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, { desc = "git [s]tage hunk" })
+        map("v", "<leader>ghr", function()
+          gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, { desc = "git [r]eset hunk" })
 
-      map("n", "<leader>ghb", function()
-        gitsigns.blame_line({ full = true })
-      end, { desc = "[g]it [b]lame line" })
+        -- normal mode
+        map("n", "]H", function()
+          gitsigns.nav_hunk("last")
+        end, { desc = "Last Hunk" })
+        map("n", "[H", function()
+          gitsigns.nav_hunk("first")
+        end, { desc = "First Hunk" })
+        map({ "n", "x" }, "<leader>ghs", gitsigns.stage_hunk, { desc = "git hunk [s]tage" })
+        map({ "n", "x" }, "<leader>ghr", gitsigns.reset_hunk, { desc = "git hunk [r]eset" })
+        map("n", "<leader>ghS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
+        map("n", "<leader>ghu", gitsigns.undo_stage_hunk, { desc = "git [u]ndo stage hunk" })
+        map("n", "<leader>ghR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" })
+        map("n", "<leader>ghp", gitsigns.preview_hunk, { desc = "git [p]review hunk" })
+        map("n", "<leader>ghi", gitsigns.preview_hunk_inline, { desc = "[T]oggle git show [D]eleted" })
 
-      map("n", "<leader>ghB", function()
-        gitsigns.blame()
-      end, { desc = "[g]it [B]lame buffer" })
+        map("n", "<leader>ghb", function()
+          gitsigns.blame_line({ full = true })
+        end, { desc = "[g]it [b]lame line" })
 
-      map("n", "<leader>ghd", gitsigns.diffthis, { desc = "git [d]iff against index" })
-      map("n", "<leader>ghD", function()
-        gitsigns.diffthis("@")
-      end, { desc = "git [D]iff against last commit" })
-      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns Select Hunk" })
-    end,
+        map("n", "<leader>ghB", function()
+          gitsigns.blame()
+        end, { desc = "[g]it [B]lame buffer" })
+
+        map("n", "<leader>gtb", gitsigns.toggle_current_line_blame, { desc = "[T]oggle git show [b]lame line" })
+
+        map("n", "<leader>ghd", gitsigns.diffthis, { desc = "git [d]iff against index" })
+        map("n", "<leader>ghD", function()
+          gitsigns.diffthis("@")
+        end, { desc = "git [D]iff against last commit" })
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns Select Hunk" })
+      end,
+    },
   },
   {
     "gitsigns.nvim",
